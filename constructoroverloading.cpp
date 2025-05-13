fd4c1ea14eb3715d28b9607a305c26f90e73818f
fadilsyy <fadilasytar@gmail.com>
2025-05-13 19:15:02 +0700

    constructoroverloading

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -24,6 +24,25 @@ mahasiswa::mahasiswa(string iNama){
      nama = iNama; //definisi hanya NAMA
 }
 
-int main(){
+mahasiswa::mahasiswa(int iNim, string iNama){
+    nim = iNim; //Definisi NIM dan NAMA
+    nama = iNama;
+}
 
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);             //hanya NIM
+    mahasiswa mhs2("Andi");             //hanya nama
+    mahasiswa mhs3(10203, "Santi");     //NIM dan nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
 }
